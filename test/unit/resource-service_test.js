a3177dd8cbc15415ce24205a3566fa7a1178b809
Daniel Moran <dmj@tid.es>
2015-11-17 15:11:50 +0100

    ADD Get resource operation

--- test/unit/resource-service_test.js
+++ test/unit/resource-service_test.js
@@ -84,7 +84,47 @@ describe('OneM2M module', function() {
         });
     });
     describe('When a user gets a resource', function() {
-        it('should return all the service information');
+        var expectedResult = {
+            rty: '4',
+            ri: 'CI00000000000000000042',
+            rn: 'testDevice',
+            pi: 'CT00000000000000000048',
+            ct: '2015-11-17T12:03:16+01:00',
+            lt: '2015-11-17T12:03:16+01:00',
+            st: '1',
+            cr: 'AE00000000000000000052',
+            cnf: 'text',
+            cs: '3',
+            con: '101',
+            rsc: '2000'
+        };
+
+        beforeEach(function(done) {
+            nock.cleanAll();
+
+            oneM2MMock = nock('http://mockedOneM2M.com:4567')
+                .matchHeader('X-M2M-RI', /^[a-f0-9\-]*$/)
+                .matchHeader('X-M2M-Origin', 'Origin')
+                .get('/Mobius/AE-SmartGondor/container-gardens/contentInstance-testDevice')
+                .reply(
+                200,
+                utils.readExampleFile('./test/unit/oneM2MResponses/resourceGetSuccess.xml', true),
+                {
+                    'X-M2M-RI': '123450e17f923-a5b0-436a-b7f2-4a17d0c1410b',
+                    'X-M2M-RSC': '2000'
+                });
+
+            configService.init(config, done);
+        });
+
+        it('should return all the information from the resource', function(done) {
+            resourceService.get('SmartGondor', 'gardens', 'testDevice', function(error, result) {
+                should.not.exist(error);
+                should.exist(result);
+                result.should.deepEqual(expectedResult);
+                done();
+            });
+        });
     });
     describe('When a user removes a resource', function() {
         it('should send an remove content instance for the selected resource to the OneM2M endpoint');
