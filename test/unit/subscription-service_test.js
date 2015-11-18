dbcac772d01509d8001a4885dded4bd9ed297c0f
Daniel Moran <dmj@tid.es>
2015-11-18 09:45:12 +0100

    ADD Subscription removal operation

--- test/unit/subscription-service_test.js
+++ test/unit/subscription-service_test.js
@@ -125,7 +125,30 @@ describe('OneM2M module: Subscriptions', function() {
         });
     });
     describe('When a user removes a subscription', function() {
-        it('should send a remove subscription request to the OneM2M endpoint');
+        beforeEach(function(done) {
+            nock.cleanAll();
+
+            oneM2MMock = nock('http://mockedOneM2M.com:4567')
+                .matchHeader('X-M2M-RI', /^[a-f0-9\-]*$/)
+                .matchHeader('X-M2M-Origin', 'Origin')['delete']('/Mobius/AE-SmartGondor/container-gardens' +
+                    '/subscription-subscription_1')
+                .reply(
+                200,
+                {
+                    'X-M2M-RI': '123450e17f923-a5b0-436a-b7f2-4a17d0c1410b',
+                    'X-M2M-RSC': '2002'
+                });
+
+            configService.init(config, done);
+        });
+
+        it('should send a remove subscription request to the OneM2M endpoint', function(done) {
+            subscriptionService.remove('SmartGondor', 'gardens', 'subscription_1', function(error, result) {
+                should.not.exist(error);
+                oneM2MMock.done();
+                done();
+            });
+        });
     });
     describe('When a notification arrives to the notification endpoint', function() {
         it('should execute the handler with the information of the notification');
