9293c03d720a81daa53525d5142bc47c941398c4
Daniel Moran <dmj@tid.es>
2015-11-25 11:03:53 +0100

    FIX Some integration problems

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ config.iota = {
     types: {},
     service: 'howtoService',
     subservice: '/howto',
-    providerUrl: 'http://localhost:4041',
+    providerUrl: 'http://localhost:4048',
     deviceRegistrationDuration: 'P1M',
     defaultType: 'Thing'
 };
